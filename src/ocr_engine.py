90a55365883f2be2c482b4a8cb4f01a7aaf3f265
shyamgadhiya <128476060+shyamgadhiya@users.noreply.github.com>
2026-01-02 14:37:38 +0530

    Refactor perform_ocr function to simplify return

--- src/ocr_engine.py
+++ src/ocr_engine.py
@@ -2,7 +2,6 @@ import pytesseract
 
 def perform_ocr(image):
     custom_config = r'--oem 3 --psm 6'
-    text = pytesseract.image_to_string(image, config=custom_config)
-    return text
+    return pytesseract.image_to_string(image, config=custom_config)
 
 
