f6390319f24a7195ec22121b373d3f6cb2b33d58
shyamgadhiya <128476060+shyamgadhiya@users.noreply.github.com>
2026-01-02 14:30:04 +0530

    Update text_extraction.py

--- src/text_extraction.py
+++ src/text_extraction.py
@@ -3,14 +3,12 @@ import regex as re
 def extract_target_line(ocr_text):
     lines = ocr_text.splitlines()
 
-    pattern = re.compile(
-        r'([A-Z0-9]+[_\-]?[1I|l][_A-Z0-9]+)', re.IGNORECASE
-    )
+    pattern = re.compile(r'.*(_1_|_1\b|\b1_).*')
 
     matches = []
     for line in lines:
         clean = line.replace(" ", "")
-        if re.search(r'_1_|_1\b|\b1_', clean) or pattern.search(clean):
+        if pattern.search(clean):
             matches.append(line.strip())
 
-    return list(set(matches))
+    return matches
